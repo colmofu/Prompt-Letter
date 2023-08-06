49ce27d56544260a39fc7d9695b6143ff1329d4e
colmofu <cofuarthain@gmail.com>
2023-08-06 17:36:29 +0200

    First attempt at creating prompt - still not functional

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Prompt-Letter
+
+## Description
+
+This is a small project for personal use in my search for work. The idea is to create an initial draft of a cover letter for potential job applications
+using prompt engineering and GPT models.
+
+The model takes in as arguments a pdf of the job posting and a pdf of the applicant's resume, analyzing it for text. GPT then uses this to construct a conventional and job/applicant-relevant cover letter.
+
+The user can also specify the occupation field and the output language (English and German).
+
+__NOTE__ : One should be aware before using this program that due to its reliance on GPT models, users are potentially passing sensitive and private personal data to OpenAI. Please take the opportunity to inform yourself about this risk and whether or not it is something you desire. All risk belongs to the user.
+
+## Installation and Usage
+
+Users require their own OPEN_API key for use of this program. I have saved mine as an environment variable.
+
+## Credits
+
+The idea for this concept belongs to Armando Cortez. I implemented my own version of his idea simply for my own personal use and using my own ideas for how it could be developed. This is simply a personal project to improve my prompt engineering skills and I have no desire to profit off it.
+
+Code is original and my own.
+
+## Requirements
+
+A list of required packages is found in the `requirements.txt` file.
+
+## Known Bugs
+
+## Future Improvements
+
+If you have ideas or improvements, please reach out! The goal of this project is to improve my prompt-engineering skills, so I'm very open to ideas.
+
+If I have time, I would like to add more user interaction and a front-end to this program.
+
+## Copyright and licensing information
+
+Original idea -> Armando Cortez. Code was created by me for my own personal use, however, it was his original idea.
+
+This project is licensed under the terms of the GNU Affero General Public License v3.0. Please see the `LICENSE.txt` for specific details.
---
+++ model.py
@@ -0,0 +1,60 @@
+import openai
+import os
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-4"):
+    """
+    Helper function to get completion from model based on a prompt input
+    """
+    
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def write_letter(job_posting, resume, lang = 'en', field = 'tech/data', *args, **kwargs):
+    """
+    Write cover letter for a job post in either English (default) or German.
+    """
+    languages = {'en': 'English',
+                 'de': 'German'}
+    
+    prompt_topic = f"""
+    You are an assistant for writing cover letters for job applications in the {field} field. You will receive the /
+    job posting (`job_posting`) and applicant's resume (`resume`) as arguments. Using these documents and your expertise as a career specialist, /
+    write a professional cover letter using sharp and field-relevant language. 
+
+    Provide the output in string format in the {languages[lang]} language. The cover letter should be 300-350 words in total. The letter should /
+    consist of a conventional format for a cover letter: /
+    - An empty skeleton area for the user to enter the mailing information of the recruiter, /
+    followed by a conventional greeting and the body split into three paragraphs.
+     - A first paragraph consisting of 2-3 sentences, mentioning the position, the company, /
+    and what attracted the applicant to the role. It should grasp the readers attention.
+     - A second paragraph convincing the reader that the applicant is the perfect fit for the role, highlighting relevant experience. Here, /
+    the letter should link relevant skills in the job-posting to compatible items found in the readers resume. It should be fact driven /
+    and lean heavily on who the applicant is.
+     - A third paragraph explaining what attracted the applicant to the company and position. Use what is provided about the role and company in the /
+    job posting, and if provided, the additional arguments.
+     - A concluding paragraph thanking the reader for their time and following a standard conclusion. 
+        
+
+    Review job_posting: '''{job_posting}'''
+    Review resume: '''{resume}'''
+    """
+
+    letter = get_completion(prompt_topic)
+
+    return letter
+
+def text_to_pdf(text):
+    """
+    return pdf for written text from a txt to pdf
+    """
+    pdf = []
+    return pdf
+
+
